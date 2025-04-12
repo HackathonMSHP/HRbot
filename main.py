67c61553137eb20d4f9a7bb0dbf70149e3cf6687
fwackit <1ceyyyForGames@gmail.com>
2025-04-12 14:49:24 +0300

    start (without sql)

--- main.py
+++ main.py
@@ -5,23 +5,20 @@ import traceback
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from dotenv import load_dotenv
 
+from handlers.start import start_router
+from handlers.employer import employer_router
+from handlers.worker import worker_router
 from utils.bot_dispatcher import create_dispatcher
 from utils.db_connector import create_tables #, delete_tables, recreate_tables
-
-load_dotenv()
-TOKEN = os.getenv("TOKEN")
-
-if not TOKEN:
-    raise ValueError("Токен не найден. Убедитесь, что вы создали файл .env с токеном.")
+from constants.config import TOKEN
 
 
 async def main() -> None:
     await create_tables()  # создаем таблицы в бд
     bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
     dp = create_dispatcher()
+    dp.include_routers(start_router, employer_router, worker_router)
     await dp.start_polling(bot)  # запускаем бота
 
 
