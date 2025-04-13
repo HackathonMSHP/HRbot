d5874367d30e933f368b5f92b4f8dcad799661a4
romariob10 <romario.b10@mail.ru>
2025-04-13 13:33:13 +0300

    Update find.py

--- utilities/find.py
+++ utilities/find.py
@@ -4,8 +4,16 @@ async def find_koef(id_worker, id_employer):
     worker = await get_worker(id_worker)
     employer = await get_employer(id_employer)
 
-    for tag in worker.tags:
-        if tag in employer.tags:
-            return 1
+    koef = 0
 
-    return 0
+    for tag in worker.tags:
+        if t in employer.tags:
+            koef += 1
+
+    if (employer.age_min <= worker.age <= employer.age_max):
+        koef += 3
+
+    if (not employer.work_experience_min <= worker.work_experience <= employer.work_experience_max):
+        koef -= 5
+
+    return koef
